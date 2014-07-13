bed79a618e7333960ddaa0210f8a0469b88c6dd8
Nick Armstrong-Crews <nick@heuristiclabs.com>
2014-07-13 18:15:38 -0400

    added upload / download scripts; need double-checking

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# FIXME
+scp -r /media/ni21806/5A24B1AC24B18B8F/Users/ni21806/Desktop/* scp -i ~/.ssh/xavier_priv_key.pem ubuntu@ec2-54-187-248-236.us-west-2.compute.amazonaws.com:~/lze_data/
